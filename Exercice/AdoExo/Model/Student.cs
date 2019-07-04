167b1386cb945a46c4650ee904823475ea73786b
Lhel-Dev <aude.beurive@gmail.com>
2019-07-04 12:10:24 +0200

    exo insert 1 & 2
    exercices d'insertion de données sans et avec param

---
+++ Exercice/AdoExo/Model/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoExo
+{
+    class Student
+    {
+        private int _Id, _SectionId;
+        private double _YearResult;
+        private string _LastName, _FirstName;
+        private DateTime _Birthdate;
+        private bool _Active;
+
+        public int Id { get; set; }
+        public string LastName { get { return _LastName; } }
+        public string FirstName { get { return _FirstName; } }
+        public DateTime BirthDate { get { return _Birthdate; }
+            set {
+                if (value >= new DateTime(1930, 1, 1)) _Birthdate = value;
+                else _Birthdate = new DateTime(1930, 1, 1); } }
+        public int SectionId { get { return _SectionId; } }
+        public double YearResult { get { return _YearResult; } set { if (value >= 0 && value <= 20) _YearResult = value; else _YearResult = 0; } }
+        public bool Active { get; set; }
+
+        public Student(string lastname, string firstname, DateTime birthdate, double yearresult, int sectionid )
+        {
+            _Id = 0;
+            _LastName = lastname;
+            _FirstName = firstname;
+            _Birthdate = birthdate;
+            _YearResult = yearresult;
+            _SectionId = sectionid;
+            _Active = true;
+        }
+    }
+}
--- Exercice/AdoExo/Program.cs
+++ Exercice/AdoExo/Program.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+
 
 namespace AdoExo
 {
@@ -13,6 +16,10 @@ namespace AdoExo
         static void Main(string[] args)
         {
             string SQLAuthConnectionString = @"Server=TECHNOBEL;" + "Database=Exercice;" + "User Id=sa;Password=test1234=;";
+            Student aude = new Student("Beurive", "Aude", new DateTime(1989, 10, 16), 19.5, 1120);
+            Student marceau = new Student("Depasse", "Marceau", new DateTime(1990, 05, 03), 20, 1010);
+            NumberFormatInfo nfi = new NumberFormatInfo();
+            nfi.NumberDecimalSeparator = ".";
 
             try
             {
@@ -20,49 +27,85 @@ namespace AdoExo
                 {
                     connect.ConnectionString = SQLAuthConnectionString;
 
-                    using (SqlCommand command = connect.CreateCommand())
-                    {                        
-                        command.CommandText = "SELECT Id, LastName, FirstName, YearResult FROM V_Student";
+                    //using (SqlCommand command = connect.CreateCommand())
+                    //{                        
+                    //    command.CommandText = "SELECT Id, LastName, FirstName, YearResult FROM V_Student";
 
-                        SqlDataAdapter da = new SqlDataAdapter();
-                        da.SelectCommand = command;
-                        DataTable ds = new DataTable();
-                        da.Fill(ds);
-                        
-                        if(ds.Rows.Count > 0)
-                        {
-                            double moyenne = 0;
-                            foreach(DataRow row in ds.Rows)
-                            {
-                                Console.WriteLine($"{row["Id"]} - {row["LastName"]} {row["FirstName"]}");
-                                moyenne += (int)row["YearResult"];
-                            }
-                            moyenne /= ds.Rows.Count;
-                            Console.WriteLine("----------");
-                            Console.WriteLine("Moyenne annuelle : " + moyenne);
-                        }
-                        Console.WriteLine("----------");
-                        connect.Open();
-                        using (SqlDataReader reader = command.ExecuteReader())
-                        {
-                            while(reader.Read())
-                            {
-                                Console.WriteLine($"{reader["Id"]} - {reader["LastName"]} {reader["FirstName"]}");
-                            }
-                        }
-                        connect.Close();
-                       
-                    }
-                    using (SqlCommand moyenne = connect.CreateCommand())
+                    //    SqlDataAdapter da = new SqlDataAdapter();
+                    //    da.SelectCommand = command;
+                    //    DataTable ds = new DataTable();
+                    //    da.Fill(ds);
+
+                    //    if(ds.Rows.Count > 0)
+                    //    {
+                    //        double moyenne = 0;
+                    //        foreach(DataRow row in ds.Rows)
+                    //        {
+                    //            Console.WriteLine($"{row["Id"]} - {row["LastName"]} {row["FirstName"]}");
+                    //            moyenne += (int)row["YearResult"];
+                    //        }
+                    //        moyenne /= ds.Rows.Count;
+                    //        Console.WriteLine("----------");
+                    //        Console.WriteLine("Moyenne annuelle : " + moyenne);
+                    //    }
+                    //    Console.WriteLine("----------");
+                    //    connect.Open();
+                    //    using (SqlDataReader reader = command.ExecuteReader())
+                    //    {
+                    //        while(reader.Read())
+                    //        {
+                    //            Console.WriteLine($"{reader["Id"]} - {reader["LastName"]} {reader["FirstName"]}");
+                    //        }
+                    //    }
+
+                    //using (SqlCommand moyenne = connect.CreateCommand())
+                    //{
+                    //    connect.Open();
+                    //    moyenne.CommandText = "SELECT AVG(CONVERT(FLOAT,YearResult)) FROM V_Student";
+                    //    double moy = (double)moyenne.ExecuteScalar();
+                    //    Console.WriteLine("----------");
+                    //    Console.WriteLine("Moyenne annuelle : "+moy);
+                    //    connect.Close();
+                    //}
+                //}
+                    //////////////////// Ajout Student Aude /////////////////////
+                    //using (SqlCommand command = connect.CreateCommand())
+                    //{
+                    //    connect.Open();                        
+                    //    command.CommandText = $"INSERT INTO Student(FirstName, LastName, BirthDate, YearResult, SectionId) OUTPUT inserted.Id" +
+                    //        $" values('{aude.FirstName}','{aude.LastName}','{aude.BirthDate.ToString("yyy-MM-dd")}',{aude.YearResult.ToString(nfi)},{aude.SectionId});";
+                    //    aude.Id = (int)command.ExecuteScalar();
+                    //    Console.WriteLine(command.CommandText);
+                    //}
+
+                    /////////////////// Ajout Student Marceau //////////////////
+                    using (SqlCommand command = connect.CreateCommand())
                     {
                         connect.Open();
-                        moyenne.CommandText = "SELECT AVG(CONVERT(FLOAT,YearResult)) FROM V_Student";
-                        double moy = (double)moyenne.ExecuteScalar();
-                        Console.WriteLine("----------");
-                        Console.WriteLine("Moyenne annuelle : "+moy);
-                        connect.Close();
+                        command.CommandText = $"INSERT INTO Student(FirstName, LastName, BirthDate, YearResult, SectionId) OUTPUT inserted.Id" +
+                            $" values(@FirstName,@LastName, @BirthDate, @YearResult, @SectionId);";
+                        SqlParameter PFirstName = new SqlParameter() { ParameterName = "FirstName", Value = marceau.FirstName };
+                        SqlParameter PLastName = new SqlParameter() { ParameterName = "LastName", Value = marceau.LastName };
+                        SqlParameter PBirthDate = new SqlParameter() { ParameterName = "BirthDate", Value = marceau.BirthDate.ToString("yyyy-MM-dd")};
+                        SqlParameter PYearResult = new SqlParameter() { ParameterName = "YearResult", Value = marceau.YearResult };
+                        SqlParameter PSectionId = new SqlParameter() { ParameterName = "SectionId", Value = marceau.SectionId };
+                        List<SqlParameter> Listparam = new List<SqlParameter>() { PLastName, PFirstName, PBirthDate, PYearResult, PSectionId };                        
+                        command.Parameters.Add(PFirstName);
+                        command.Parameters.Add(PLastName);
+                        command.Parameters.Add(PBirthDate);
+                        command.Parameters.Add(PYearResult);
+                        command.Parameters.Add(PSectionId);
+
+                        marceau.Id = (int)command.ExecuteScalar();
+                        Console.WriteLine(command.CommandText);
                     }
+
+
+                    connect.Close();
+                       
+                    
                 }
+                
             }
             catch (Exception e)
             {
